de352333ea24e025ba337ec2b46198676bedacec
lucamoynierUBC <lmoynier@student.ubc.ca>
2024-02-22 17:14:13 -0800

    need to fix ant direction bug

--- app/Main.hs
+++ app/Main.hs
@@ -31,7 +31,7 @@ cellDimension :: Int
 cellDimension = 5
 
 window :: Display
-window = InWindow "Langtons Ant" (gridDimension * cellDimension, gridDimension * cellDimension) (10, 10)
+window = InWindow "Langtons Ant" (gridDimension * cellDimension, gridDimension * cellDimension) (100, 100)
 
 -- Drawing Settings
 background :: Color
@@ -41,7 +41,7 @@ antRadius :: Integer
 antRadius = 2
 
 spawnAnt :: Ant
-spawnAnt = ((gridDimension `div` 2, gridDimension `div` 2), W)
+spawnAnt = ((gridDimension `div` 2, gridDimension `div` 2), N)
 
 turnLeft :: Direction -> Direction
 turnLeft N = W
@@ -52,7 +52,7 @@ turnLeft E = N
 turnRight :: Direction -> Direction
 turnRight N = E
 turnRight E = S
-turnRight S = W 
+turnRight S = W
 turnRight W = N
 
 flipCell :: Cell -> Cell
@@ -65,7 +65,7 @@ initialState = Game
     {
         antPosition = (gridDimension `div` 2, gridDimension `div` 2),
         steps = 0,
-        antState = W,
+        antState = N,
         gridState = replicate gridDimension (replicate gridDimension White)
     }
 
@@ -85,40 +85,53 @@ drawGrid grid = Pictures [ translate (fromIntegral x * cellSize) (fromIntegral y
             borderColor = greyN 0.8 -- Light grey color for the border
 
 drawAnt :: Ant -> Picture
-drawAnt ((x, y), _) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) $ color red $ circleSolid antSize
+drawAnt ((x, y), dir) = translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) $ rotate (directionToAngle dir) $ color red $ polygon trianglePoints
   where
     cellSize = fromIntegral cellDimension
     antSize = fromIntegral antRadius
+    trianglePoints = [(-antSize, -antSize/2), (antSize, 0), (-antSize, antSize/2)]
+
+    directionToAngle :: Direction -> Float
+    directionToAngle N = 0
+    directionToAngle E = 90
+    directionToAngle S = 180
+    directionToAngle W = -90
 
 drawing :: Gamestate -> Picture
 drawing gameState = pictures [drawGrid (gridState gameState), drawAnt (antPosition gameState, antState gameState)]
 
-
-
--- Make sure fps is defined properly, e.g., fps = 60
 fps :: Int
-fps = 30
+fps = 1
 
--- Corrected moveAnt function to move ant by 1 unit disregarding seconds
 moveAnt :: Float -> Gamestate -> Gamestate
-moveAnt _ game = game { antPosition = newPos,  antState = newDir, gridState = newGrid}
+moveAnt _ game = game { antPosition = (x', y'),  antState = newDir, gridState = newGrid, steps = (steps game) + 1}
   where
     grid = gridState game
     dir = antState game
     (x, y) = antPosition game
-    currCell = (grid !! x) !! y
-    newDir = if currCell == Black then turnRight dir else turnLeft dir
-    newPos  | newDir == N = (x, (y-1) `mod` gridDimension)
-            | newDir == S = (x, (y+1) `mod` gridDimension)
-            | newDir == W = ((x-1) `mod` gridDimension, y)
-            | newDir == E = ((x+1) `mod` gridDimension, y)
-    newGrid = updateGrid x y (flipCell currCell) grid
+    currCell = (grid !! y) !! x
+    newDir  | currCell == Black = turnLeft dir
+            | otherwise = turnRight dir
+    x'  | newDir == E = x + 1
+        | newDir == W  = x - 1
+        | otherwise = x
+    y'  | newDir == N = y - 1
+        | newDir == S = y + 1
+        | otherwise = y
+    newGrid = updateGrid x y currCell grid
+
+
 
 updateGrid :: Int -> Int -> Cell -> Grid -> Grid
-updateGrid x y newCell grid = 
-  take y grid ++ 
-  [take x (grid !! y) ++ [newCell] ++ drop (x+1) (grid !! y)] ++ 
-  drop (y+1) grid
+updateGrid x y cell grid = map updateRow (zip [0..] grid)
+  where
+    updateRow (rIdx, row)
+      | rIdx == y = updateCol row
+      | otherwise = row
+    updateCol row = map updateCell (zip [0..] row)
+    updateCell (cIdx, cell)
+      | cIdx == x = flipCell cell
+      | otherwise = cell
     
 
     
